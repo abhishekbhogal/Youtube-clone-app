243704b1ec852338175168f4412ac1cc81dbe22c
Abhishek Bhogal <abhishek.bhogal7@gmail.com>
2024-04-08 10:19:01 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "key_name" {
+  description = "The name of the SSH key pair to use for the instances"
+}
+
+variable "private_key_path" {
+  description = "The local path to the private key file"
+}
